43d66da62fa8d630b0a8d73442843ba654eed958
amirrezabahramy <amirrezabahramy@gmail.com>
2024-01-08 11:43:06 +0330

    Separated client provider to query client provider and auth provider

---
+++ src/contexts/AuthProvider.tsx
@@ -0,0 +1,71 @@
+import { removeHeader, setHeader } from "@/services/api";
+import { TUser } from "@/types/models";
+import { typedLocalStorage } from "@/utils/helpers/typed-local-storage";
+import { useNavigate } from "@tanstack/react-router";
+import { jwtDecode } from "jwt-decode";
+import { createContext, useContext, useState } from "react";
+
+// Types
+type TProps = {
+  children: React.ReactNode;
+};
+
+type TLoggedInUser =
+  | (TUser & {
+      exp: number;
+      iat: number;
+    })
+  | null;
+type TLoginUser = (token: string) => void;
+type TLogoutUser = () => void;
+
+type TAuthContext = {
+  loggedInUser: TLoggedInUser;
+  loginUser: TLoginUser;
+  logoutUser: TLogoutUser;
+};
+
+// Context
+const AuthContext = createContext<TAuthContext>({
+  loggedInUser: null,
+  loginUser: () => {},
+  logoutUser: () => {},
+});
+
+// Setting token on page load
+const token = typedLocalStorage.getItem("token");
+token && setHeader("Authorization", `Bearer ${token}`);
+
+function AuthProvider({ children }: TProps) {
+  // Logged in user management
+  const [loggedInUser, setLoggedInUser] = useState<TLoggedInUser>(
+    JSON.parse(typedLocalStorage.getItem("loggedInUser") || "null")
+  );
+
+  const logoutUser = () => {
+    removeHeader("Authorization");
+    typedLocalStorage.removeItem("token");
+    typedLocalStorage.removeItem("loggedInUser");
+    setLoggedInUser(null);
+  };
+
+  const loginUser: TLoginUser = (token) => {
+    const user: TLoggedInUser = jwtDecode(token);
+    setHeader("Authorization", `Bearer ${token}`);
+    typedLocalStorage.setItem("token", token);
+    typedLocalStorage.setItem("loggedInUser", JSON.stringify(user));
+    setLoggedInUser(user);
+  };
+
+  return (
+    <AuthContext.Provider value={{ loggedInUser, loginUser, logoutUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export default AuthProvider;
---
+++ src/contexts/QueryClientProvider.tsx
@@ -0,0 +1,60 @@
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider as TanstackQueryClientProvider,
+} from "@tanstack/react-query";
+import { useEffect, useMemo } from "react";
+import useQueryHandler from "@/hooks/useQueryHandler";
+
+import { AxiosError, AxiosResponse } from "axios";
+import { useAuth } from "./AuthProvider";
+
+type TProps = {
+  children: React.ReactNode;
+};
+
+function QueryClientProvider({ children }: TProps) {
+  // Hooks
+  const { handleSuccess, handleError } = useQueryHandler();
+  const { loggedInUser, logoutUser } = useAuth();
+
+  // Query client setup
+  const queryClient = useMemo(
+    () =>
+      new QueryClient({
+        queryCache: new QueryCache({
+          onError: (error) => {
+            handleError(error as AxiosError, logoutUser);
+          },
+        }),
+        defaultOptions: {
+          mutations: {
+            onSuccess: (data) => {
+              handleSuccess(data as AxiosResponse);
+            },
+            onError: (error) => {
+              handleError(error as AxiosError, logoutUser);
+            },
+          },
+        },
+      }),
+    []
+  );
+
+  useEffect(() => {
+    const cleanUp = () => {
+      if (!loggedInUser) {
+        queryClient.removeQueries();
+      }
+    };
+    cleanUp();
+  }, [loggedInUser]);
+
+  return (
+    <TanstackQueryClientProvider client={queryClient}>
+      {children}
+    </TanstackQueryClientProvider>
+  );
+}
+
+export default QueryClientProvider;
