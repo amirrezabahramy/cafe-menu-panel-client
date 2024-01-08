3853ce2528153b2828fcf29c88c7497e7cbb6ee0
amirrezabahramy <amirrezabahramy@gmail.com>
2024-01-08 18:41:51 +0330

    New component to render order items list

---
+++ src/components/generic/OrderItemsList.tsx
@@ -0,0 +1,90 @@
+import api from "@/services/api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import AsyncComponent from "../generic/AsyncComponent";
+import { AxiosError } from "axios";
+import OrderItemCardEditable from "./OrderItemCardEditable.tsx";
+import FlexListContainer from "../generic/FlexListContainer";
+import { TPatchOrderItem } from "@/types/models/patch-models";
+import {
+  TOrderItemInstanceData,
+  TOrderItemListData,
+} from "@/types/queries/order-item.ts";
+import { TPostOrderItem } from "@/types/models/post-models.ts";
+import OrderItemCardAdder from "./OrderItemCardAdder.tsx";
+
+type TProps = {
+  url: "food" | "hot-drinks" | "cold-drinks";
+};
+
+function OrderItemsList({ url }: TProps) {
+  const queryClient = useQueryClient();
+
+  // Get all
+  const {
+    isLoading,
+    isError,
+    error,
+    data: orderItems,
+  } = useQuery({
+    queryKey: [`get-all-${url}`],
+    queryFn: () => api.get<void, TOrderItemListData>(url),
+    select: (data) => data.data,
+  });
+
+  // Add
+  const { mutateAsync: addOrderItem } = useMutation({
+    mutationKey: [`add-${url}`],
+    mutationFn: (data: TPostOrderItem) =>
+      api.post<TPostOrderItem, TOrderItemInstanceData>(url, data),
+    onSettled: async () =>
+      await queryClient.invalidateQueries({ queryKey: [`get-all-${url}`] }),
+  });
+
+  // Update
+  const { mutateAsync: updateOrderItem } = useMutation({
+    mutationKey: [`update-${url}`],
+    mutationFn: (data: TPatchOrderItem) =>
+      api.patch<TPatchOrderItem, TOrderItemInstanceData>(
+        `${url}/${data._id}`,
+        data
+      ),
+    onSettled: async () =>
+      await queryClient.invalidateQueries({ queryKey: [`get-all-${url}`] }),
+  });
+
+  // Remove
+  const { isPending: isRemoving, mutateAsync: removeOrderItem } = useMutation({
+    mutationKey: [`remove-${url}`],
+    mutationFn: (id: string) =>
+      api.delete<string, TOrderItemInstanceData>(`${url}/${id}`),
+    onSettled: async () =>
+      await queryClient.invalidateQueries({ queryKey: [`get-all-${url}`] }),
+  });
+
+  return (
+    <AsyncComponent
+      isLoading={isLoading}
+      isError={isError}
+      error={error as AxiosError}
+    >
+      <FlexListContainer>
+        <OrderItemCardAdder onAdd={addOrderItem} />
+        {orderItems?.map((orderItem) => (
+          <OrderItemCardEditable
+            key={orderItem._id}
+            id={orderItem._id}
+            name={orderItem.name}
+            image={orderItem.image}
+            description={orderItem.description}
+            price={orderItem.price}
+            onRemove={removeOrderItem}
+            onUpdate={updateOrderItem}
+            isRemoving={isRemoving}
+          />
+        ))}
+      </FlexListContainer>
+    </AsyncComponent>
+  );
+}
+
+export default OrderItemsList;
