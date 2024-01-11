8b545e004b36b015067534f542ef46d63aa10276
amirrezabahramy <amirrezabahramy@gmail.com>
2024-01-11 22:51:56 +0330

    Code cleanup

--- src/utils/validators/index.ts
+++ src/utils/validators/index.ts
@@ -5,5 +5,6 @@ export const required = () => z.string().trim().min(1, REQUIRED);
 
 export const nameValidator = () =>
   z.string().trim().min(4, lengthError("نام", 4, "min"));
+
 export const descriptionValidator = () =>
   z.string().trim().min(10, lengthError("توضیحات", 10, "min"));
